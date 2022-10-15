efcf1a9149b29952014e4a4ecc1f2cde6666088e
Anton Molchanov <mo4anov.a@gmail.com>
2022-10-15 17:35:02 +0300

    is done

--- homework.py
+++ homework.py
@@ -69,7 +69,7 @@ class Running(Training):
                 + self.CALORIES_MEAN_SPEED_SHIFT)
                 * self.weight
                 / self.M_IN_KM
-                * (self.duration * self.MIN_IN_H))
+                * self.duration * self.MIN_IN_H)
 
 
 class SportsWalking(Training):
